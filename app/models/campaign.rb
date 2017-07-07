0d9e3ab92b2221335e3121758c0b3458fe8559dd
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-07-07 14:08:28 +0200

    forest

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,8 +2,4 @@ class Campaign < ApplicationRecord
   #associations
   belongs_to :user
   has_many :ad_bookings
-  #validations
-  validates :name, presence: true
-  validates :budget, presence: true
-  validates :start_date, presence: true
 end
--- app/models/radio.rb
+++ app/models/radio.rb
@@ -1,5 +1,5 @@
 class Radio < ApplicationRecord
   belongs_to :ad
   belongs_to :target
-  has_one :publisher, through: :ads
+  has_one :publisher, through: :ad
 end
